fbdc72e5bfb20699dbfc67a7a6ac5ec095822014
personal <s414june@gmail.com>
2023-01-31 16:12:47 +0800

    修改vite.config/增加環境變數

--- chatbot-qingyunke/vite.config.js
+++ chatbot-qingyunke/vite.config.js
@@ -1,26 +1,58 @@
 import {
-  defineConfig
+  defineConfig,
+  loadEnv
 } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://api.qingyunke.com/api.php',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, './'),
-        secure: false
-      },
-      '/convert': {
-        target: 'http://api.zhconvert.org/convert',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/convert/, './'),
-        secure: false
+// export default defineConfig({
+//   plugins: [vue()],
+//   server: {
+//     proxy: {
+//       './api': {
+//         target: 'http://api.qingyunke.com/api.php',
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, ''),
+//         secure: false
+//       },
+//       './convert': {
+//         target: 'http://api.zhconvert.org/convert',
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/convert/, ''),
+//         secure: false
+//       }
+//     }
+//   },
+//   base: '/ChatBot_QingYunKe/'
+// })
+
+export default ({
+  mode
+}) => {
+  // Load app-level env vars to node-level env vars.
+  process.env = {
+    ...process.env,
+    ...loadEnv(mode, process.cwd())
+  };
+
+  return defineConfig({
+    plugins: [vue()],
+    server: {
+      proxy: {
+        './api': {
+          target: 'http://api.qingyunke.com/api.php',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, process.env.ROOT_PATH),
+          secure: false
+        },
+        './convert': {
+          target: 'http://api.zhconvert.org/convert',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/convert/, process.env.ROOT_PATH),
+          secure: false
+        }
       }
-    }
-  },
-  base: '/ChatBot_QingYunKe/'
-})
+    },
+    base: process.env.ROOT_PATH
+  });
+}
