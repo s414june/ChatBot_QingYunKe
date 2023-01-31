779e33e34b4bc93ceecd22948ae89953722a51f0
personal <s414june@gmail.com>
2023-01-31 16:51:19 +0800

    api改成相對路徑2

--- chatbot-qingyunke/src/App.vue
+++ chatbot-qingyunke/src/App.vue
@@ -7,14 +7,6 @@ import UserMsg from "./components/UserMsg.vue";
 import { getCurrentInstance, ref } from "vue";
 
 let { proxy } = getCurrentInstance();
-const msgBefore = `
-<svg
-   xmlns="http://www.w3.org/2000/svg"
-   viewBox="0 0 50.663574 57.648159">
-  <path
-     d="m 0,0 c 3.6888711,27.104435 14.35237,40.733117 23.988199,47.523198 9.745755,6.867536 26.675376,10.124961 26.675376,10.124961 V 19.849951 c 0,0 -10.120617,-0.998355 -15.88017,-2.40657 C 24.472095,14.922259 11.634513,10.781255 0,0 Z"/>
-</svg>
-`;
 const textList = ref([{
   isStart: true,
   msg: "哈囉~跟我說說話吧！",
